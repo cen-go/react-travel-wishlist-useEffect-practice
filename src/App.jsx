2ee440ae879fac54cf8d6199a28784acfb692fff
luzumsuzadam <cengizulutas123123@gmail.com>
2024-09-08 23:43:37 +0300

    local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,47 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
 import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
+import { sortPlacesByDistance } from './loc.js';
+
+
+// this code runs synchronously unlike the navigator api below. it doesn't have 
+// any chance to be not executed before other code in app component finishes execution
+// and it also don't make any state management that can cause infinite loop
+// so no need to use useEffect hook there.
+const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+const storedPlaces = storedIds.map((id) =>
+  AVAILABLE_PLACES.find((place) => place.id === id)
+);
+
 
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
-  const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [availablePlaces, setAvailablePlaces ] = useState([]);
+  const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
+
+  //this function will be re-executed each time app component re-renders. 
+  // and because this function cahanges state it will cause a re-render of
+  // app component itself too, thus causing an infinite loop. Use effect hook 
+  // prevents this by depending this code's execution on a dependencies array.
+  // and also because of useEffect hook now this function will be executed after
+  // everything else in the app component finished execution.
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(
+        AVAILABLE_PLACES,
+        position.coords.latitude,
+        position.coords.longitude
+      );
+      setAvailablePlaces(sortedPlaces);
+    });
+  }, []);
+
+  
 
   function handleStartRemovePlace(id) {
     modal.current.open();
@@ -28,6 +60,18 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+
+    // in this case despite localStorage is also a side effect. this code will not
+    // cause an infinite loop. because this code doesn't cpntain any state change 
+    // so will not cause a re-render, and also this function will also won't
+    // be executed unless the button is clicked; not when the app component re-rendered.
+    const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+    if (storedIds.indexOf(id) === -1) {
+      localStorage.setItem(
+        "selectedPlaces",
+        JSON.stringify([id, ...storedIds])
+      );
+    }
   }
 
   function handleRemovePlace() {
@@ -35,6 +79,12 @@ function App() {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
     modal.current.close();
+
+    const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+    localStorage.setItem(
+      "selectedPlaces",
+      JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current))
+    );
   }
 
   return (
@@ -63,8 +113,9 @@ function App() {
         />
         <Places
           title="Available Places"
-          places={AVAILABLE_PLACES}
+          places={availablePlaces}
           onSelectPlace={handleSelectPlace}
+          fallbackText={"Fetching places sorted by your location"}
         />
       </main>
     </>
